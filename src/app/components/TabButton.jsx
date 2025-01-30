69b3f32c8a32b3a306549cc9bc2a14cea791c600
Christopher Sibanda <c9sibanda@gmail.com>
2025-01-30 11:37:23 +0000

    updated Tab Buttons to maatch theme syles

--- src/app/components/TabButton.jsx
+++ src/app/components/TabButton.jsx
@@ -1,21 +1,14 @@
-// import React from "react";
-// import { motion } from "framer-motion";
+import React from "react";
 
-// const tabButtonStyles = {
-//   base: 'px-4 py-2 mb-2 sm:mb-0 sm:mr-2 border-purple-500 border border-4 rounded-lg',
-//   active: 'bg-purple-500 text-white border-4 rounded-lg',
-//   inactive:
-//     'text-white-500 hover:bg-purple-500 hover:text-white border-4 rounded-lg',
-// };
 const tabButtonStyles = {
-  base: 'px-4 py-2 mb-2 sm:mb-0 sm:mr-2 border-4 rounded-lg',
+  base: 'px-4 py-2 mb-2 sm:mb-0 sm:mr-2 rounded-lg transition-all duration-300 bg-black border-80 border-gradient-to-br from-blue-500 via-cyan-500 to-green-500', // Gradient border, black background
   active:
-    'bg-gradient-to-r from-blue-500 via-cyan-500 to-green-500 text-white border-4 rounded-lg',
+    'bg-gradient-to-br from-blue-500 via-cyan-500 to-green-500 text-white border-transparent rounded-lg', // Active styles
   inactive:
-    'text-white-500 border-gradient border hover:bg-gradient-to-r from-blue-500 via-cyan-500 to-green-500 hover:text-white rounded-lg',
+    'text-white hover:text-white hover:bg-gradient-to-br hover:from-blue-500 hover:via-cyan-500 hover:to-green-500 rounded-lg', // Hover styles
 };
 
-// Inside your TabButton component relative group text-center text-white px-2 py-2 bg-gradient-to-r from-blue-500 via-cyan-500 to-green-500 animate-shine border-4 rounded-lg
+
 const TabButton = ({ selectTab, active, children }) => {
   const styles = active
     ? `${tabButtonStyles.base} ${tabButtonStyles.active}`
