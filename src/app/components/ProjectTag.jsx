8537660f145b6d70b2daf52c21fd3f2d2e44bc2d
Christopher Sibanda <c9sibanda@gmail.com>
2025-01-30 11:36:12 +0000

    updated Project Tags to maatch theme syles

--- src/app/components/ProjectTag.jsx
+++ src/app/components/ProjectTag.jsx
@@ -2,11 +2,12 @@ import React from "react";
 
 const ProjectTag = ({ name, onClick, isSelected }) => {
   const buttonStyles = isSelected
-    ? "text-white border-primary-500"
-    : "text-[#ADB7BE] border-slate-600 hover:border-white";
+    ? "text-white bg-gradient-to-br from-blue-500 via-cyan-500 to-green-500" // Active styles with gradient background
+    : "text-[#ADB7BE]  hover:text-white hover:border-white hover:bg-gradient-to-br hover:from-blue-500 hover:via-cyan-500 hover:to-green-500"; // Hover and default styles
+
   return (
     <button
-      className={`${buttonStyles} rounded-full border-2 px-6 py-3 text-xl cursor-pointer`}
+      className={`${buttonStyles} rounded-full border-2 px-6 py-3 text-xl cursor-pointer transition-all duration-300`} // Added transition
       onClick={() => onClick(name)}
     >
       {name}
