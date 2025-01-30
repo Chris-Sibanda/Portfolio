54bf7ab63a3657fb7e4fe9a31f19b7ee70c1d6a5
Christopher Sibanda <c9sibanda@gmail.com>
2025-01-30 11:38:35 +0000

    updated Nav bar to draw attention to important information

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -1,87 +1,25 @@
-'use client';
-import Link from 'next/link';
-import React, { useState, useEffect } from 'react';
-import NavLink from './NavLink';
-import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/solid';
-import MenuOverlay from './MenuOverlay';
-import Image from 'next/image';
+import React from "react";
 
-const navLinks = [
-  {
-    title: 'About',
-    path: '#about-title',
-  },
-  {
-    title: 'Projects',
-    path: '#projects',
-  },
-  {
-    title: 'Contact',
-    path: '#contact',
-  },
-];
+const tabButtonStyles = {
+  base: 'px-4 py-2 mb-2 sm:mb-0 sm:mr-2 rounded-lg transition-all duration-300 bg-black border-80 border-gradient-to-br from-blue-500 via-cyan-500 to-green-500', // Gradient border, black background
+  active:
+    'bg-gradient-to-br from-blue-500 via-cyan-500 to-green-500 text-white border-transparent rounded-lg', // Active styles
+  inactive:
+    'text-white hover:text-white hover:bg-gradient-to-br hover:from-blue-500 hover:via-cyan-500 hover:to-green-500 rounded-lg', // Hover styles
+};
 
-const Navbar = () => {
-  const [navbarOpen, setNavbarOpen] = useState(false);
-  const [navbarHeight, setNavbarHeight] = useState(0);
 
-  // Calculate and update the navbar height on component mount
-  useEffect(() => {
-    const navbarElement = document.querySelector('nav');
-    if (navbarElement) {
-      setNavbarHeight(navbarElement.clientHeight);
-    }
-  }, []);
+// Inside your TabButton component relative group text-center text-white px-2 py-2 bg-gradient-to-r from-blue-500 via-cyan-500 to-green-500 animate-shine border-4 rounded-lg
+const TabButton = ({ selectTab, active, children }) => {
+  const styles = active
+    ? `${tabButtonStyles.base} ${tabButtonStyles.active}`
+    : `${tabButtonStyles.base} ${tabButtonStyles.inactive}`;
 
   return (
-    <nav className="fixed mx-auto border border-[#33353F] top-0 left-0 right-0 z-10 bg-black bg-opacity-100 z-[11]">
-      <div className="flex container lg:py-4 flex-wrap items-center justify-between mx-auto px-4 py-2">
-        <Link
-          href={'/'}
-          className="text-2xl md:text-5xl text-white font-semibold"
-        >
-          <Image
-            alt="logo image"
-            src="/images/logofinal.png"
-            width={70}
-            height={70}
-            priority
-            // layout="fixed"
-          />
-        </Link>
-        <div className="mobile-menu block md:hidden">
-          {!navbarOpen ? (
-            <button
-              onClick={() => setNavbarOpen(true)}
-              className="flex items-center px-3 py-2 border rounded border-slate-200 text-slate-200 hover:text-white hover:border-white"
-            >
-              <Bars3Icon className="h-5 w-5" />
-            </button>
-          ) : (
-            <button
-              onClick={() => setNavbarOpen(false)}
-              className="flex items-center px-3 py-2 border rounded border-slate-200 text-slate-200 hover:text-white hover:border-white"
-            >
-              <XMarkIcon className="h-5 w-5" />
-            </button>
-          )}
-        </div>
-        <div className="menu hidden md:block md:w-auto" id="navbar">
-          <ul className="flex p-4 md:p-0 md:flex-row md:space-x-8 mt-0">
-            {navLinks.map((link, index) => (
-              <li key={index}>
-                <NavLink
-                  href={link.path === '#about' ? `/${link.path}` : link.path}
-                  title={link.title}
-                />
-              </li>
-            ))}
-          </ul>
-        </div>
-      </div>
-      {navbarOpen ? <MenuOverlay links={navLinks} /> : null}
-    </nav>
+    <button className={styles} onClick={selectTab}>
+      {children}
+    </button>
   );
 };
 
-export default Navbar;
+export default TabButton;
